bf4923b4313fa4c6b2e6d063c7c453885c8d7573
JanhaviJain <janhavijain0211@gmail.com>
2025-05-23 12:13:41 +0530

    kaban board

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 import BoardLayout from './components/Board';
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 import { deleteBoard, loadTasks } from './utils/boardfetch';
 
 function App() {
+  const [showAlert, setShowAlert] = useState(true);
+
   const claerBoard = () => {
     deleteBoard();
     loadTasks();
@@ -12,6 +16,16 @@ function App() {
   return (
     <div style={{maxWidth: "100%"}}>
       <h2 className='p-4 bg-dark text-white text-center'>Kaban Board</h2>
+      {showAlert && (
+        <Alert
+          variant="primary"
+          dismissible
+          onClose={() => setShowAlert(false)}
+          className='m-2'
+        >
+          Tasks are stored in the browser and may be lost if cache is cleared.
+        </Alert>
+      )}
       <BoardLayout/>
       <div className='col text-center mt-4'>
         <Button variant='danger' onClick={claerBoard}>Clear Board</Button>
